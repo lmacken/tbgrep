f87dccb7b9ae3344c30f2cc3275e30161b47536a
Luke Macken <lmacken@redhat.com>
2011-09-06 23:06:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Traceback Grep
+
+A module & command-line tool for extracting Python tracebacks text.
+
+
+##  Extracting tracebacks from bunch of files
+
+    $ python tbgrep.py file1 file2 file3
+
+## Grepping for tracebacks in a pipeline
+
+    $ tail -f logfile | python tbgrep.py
+
+## Displaying all unique tracebacks ordered by the number of occurrences
+
+    $ python tbgrep.py --stats logfile
+    [...]
+    
+    == 99 occurences ==================================================
+    
+    Traceback (most recent call last):
+     File "/usr/lib/python2.4/site-packages/bodhi/admin.py", line 209, in _masher_request
+       req_params=kwargs)
+     File "/usr/lib/python2.4/site-packages/fedora/client/proxyclient.py", line 285, in send_request
+       raise AuthError(_('Unable to log into server.  Invalid'
+    AuthError: Unable to log into server.  Invalid authentication tokens.  Send new username and password
+    
+    ==================================================================
+    733 unique tracebacks extracted
+
+## Using the Python API
+
+Once instantiated, you pass each line to the `process` method, which will
+return either None, or a string of a traceback.
+
+```python
+from tbgrep import TracebackGrep
+
+extractor = TracebackGrep()
+for line in file('logfile'):
+   tb = extractor.process(line)
+   if tb:
+       print tb
+```
+
+Instead of displaying each traceback found in the file, tbgrep also
+supports generating statistics about all tracebacks in the file.
+
+```python
+extractor = TracebackGrep(stats=True)
+for line in file('logfile'):
+   extractor.process(line)
+extractor.print_stats()
+```
+
+## Supported Input Formats
+
+tbgrep can extract tracebacks from logs of various formats. For example,
+CherryPy starts the traceback on a line with other details (like module,
+timestamp, etc), but the rest of the trace starts at the beginning of the
+line. Apache logs, on the other hand, will prefix each line of the
+traceback with this information. tbgrep is designed to handle both of
+these types of situations.
---
+++ tbgrep.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# tbgrep - Python Traceback Extractor
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2011 Luke Macken <lmacken@redhat.com>
+
+import sys
+import fileinput
+
+from collections import defaultdict
+from operator import itemgetter
+
+tb_head = 'Traceback (most recent call last):'
+
+class TracebackGrep(object):
+    tb = index = None
+    stats = firstline = prefix = False
+    tracebacks = defaultdict(int)
+
+    def __init__(self, stats=False):
+        self.stats = stats
+
+    def process(self, line):
+        if self.tb:
+            if line:
+                if self.firstline:
+                    self.prefix = line[0] != ' '
+                    self.firstline = False
+                if self.prefix:
+                    line = line[self.index:]
+                self.tb += line
+                if line and line[0] != ' ':
+                    tb = self.tb
+                    self.tb = None
+                    if self.stats:
+                        self.tracebacks[tb] += 1
+                    return tb
+        elif tb_head in line:
+            self.index = line.index(tb_head)
+            self.tb = line[self.index:]
+            self.firstline = True
+
+    def get_stats(self):
+        return sorted(self.tracebacks.items(), key=itemgetter(1))
+
+    def print_stats(self):
+        stats = extractor.get_stats()
+        for tb, num in stats:
+            print ('== %d occurences ' + '=' * 50) % num
+            print
+            print tb
+        print '=' * 80
+        print "%d unique tracebacks extracted" % len(stats)
+
+
+if __name__ == '__main__':
+    stats = False
+    if '--stats' in sys.argv:
+        stats = True
+        sys.argv.remove('--stats')
+
+    extractor = TracebackGrep(stats=stats)
+
+    for line in fileinput.input():
+        tb = extractor.process(line)
+        if not stats and tb:
+            print tb
+    if stats:
+        extractor.print_stats()
